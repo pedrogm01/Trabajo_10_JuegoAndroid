0395be48129844796ecb7dcec99d720560ecb3d8
pedrogm01 <111323643+pedrogm01@users.noreply.github.com>
2024-05-07 20:02:48 -0600

    Add files via upload

---
+++ android/build/assets/HUD.gd
@@ -0,0 +1,31 @@
+extends CanvasLayer
+
+signal start_game
+
+
+func show_message(text):
+	$MessageLabel.text = text
+	$MessageLabel.show()
+	$MessageTimer.start()
+
+
+func show_game_over():
+	show_message("Game Over")
+	await $MessageTimer.timeout
+	$MessageLabel.text = "Trabajo 10\nPedro González"
+	$MessageLabel.show()
+	await get_tree().create_timer(1).timeout
+	$StartButton.show()
+
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+
+func _on_StartButton_pressed():
+	$StartButton.hide()
+	emit_signal("start_game")
+
+
+func _on_MessageTimer_timeout():
+	$MessageLabel.hide()
